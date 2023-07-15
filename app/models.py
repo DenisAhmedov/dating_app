2302ade915c273a14d4de40fa565780a74ecf6d6
den_is_akhmedov <denis.akhmedov86@gmail.com>
2023-07-15 13:50:39 +0300

    Создание модели пользователя

--- app/models.py
+++ app/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    avatar = models.ImageField(null=True, blank=True, upload_to="avatars/")
+    sex = models.CharField(max_length=1,
+                           choices=[('m', 'Male'), ('f', 'Female')],
+                           default='m')
+
+    def __str__(self):
+        return f'{self.username}'
+
+
