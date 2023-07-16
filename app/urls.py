b0db2bdb830eb129b78fabd8a6593e62c36e4d53
den_is_akhmedov <denis.akhmedov86@gmail.com>
2023-07-16 19:45:41 +0300

    Удаление слешей в конце эндпонтов

--- app/urls.py
+++ app/urls.py
@@ -4,10 +4,10 @@ from rest_framework.authtoken.views import obtain_auth_token
 from app.views import ClientCreateView, Logout, GetAllClients, ClientView, MatchView
 
 urlpatterns = [
-    path('login/', obtain_auth_token, name='login'),
-    path('logout/', Logout.as_view(), name='logout'),
-    path('clients/create/', ClientCreateView.as_view(), name='signup'),
-    path('clients/', GetAllClients.as_view(), name='clients_list'),
-    path('clients/<int:pk>/', ClientView.as_view()),
-    path('clients/<int:pk>/match/', MatchView.as_view(), name='match'),
+    path('login', obtain_auth_token, name='login'),
+    path('logout', Logout.as_view(), name='logout'),
+    path('clients/create', ClientCreateView.as_view(), name='signup'),
+    path('list/', GetAllClients.as_view(), name='clients_list'),
+    path('clients/<int:pk>', ClientView.as_view()),
+    path('clients/<int:pk>/match', MatchView.as_view(), name='match'),
 ]
